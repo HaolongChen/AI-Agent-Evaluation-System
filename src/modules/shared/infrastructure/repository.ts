ddaed77250b07ecc3c28125c60790bf54efef05b
chenhaolong <chenhaolong@functorz.com>
2026-05-26 07:58:25 +0800

    style: format code for improved readability and consistency

--- src/modules/shared/infrastructure/repository.ts
+++ src/modules/shared/infrastructure/repository.ts
@@ -1,45 +1,53 @@
 import type z from "zod";
 import type { Entity } from "../domain/entity/entity.ts";
 import type {
-  BaseOptions,
-  BaseOptionsReturnType,
+	BaseOptions,
+	BaseOptionsReturnType,
 } from "../domain/interface/repository.interface.ts";
 
 export interface ExternalRepositoryDate {
-  createdAt?: z.infer<z.ZodDate>;
-  updatedAt?: z.infer<z.ZodDate>;
+	createdAt?: z.infer<z.ZodDate>;
+	updatedAt?: z.infer<z.ZodDate>;
 }
 
 export function repositoryDateMapper<T extends z.ZodObject>(
-  data: ExternalRepositoryDate,
-  entity: Entity<T>,
+	data: ExternalRepositoryDate,
+	entity: Entity<T>,
 ) {
-  entity.setData({ createdAt: data.createdAt, updatedAt: data.updatedAt });
-  return entity;
+	entity.setData({ createdAt: data.createdAt, updatedAt: data.updatedAt });
+	return entity;
 }
 
 export const optionsToInclude = <
-  T extends string = string,
-  E extends string = string,
-  U extends string = string,
+	T extends string = string,
+	E extends string = string,
+	U extends string = string,
 >(
-  options: BaseOptions<T, E, U>,
+	options: BaseOptions<T, E, U>,
 ) => {
-  if (!options.options) {
-    return { [options.name]: true } as const;
-  }
-  const include: Record<string, unknown> = {};
-  for (const [key, value] of Object.entries(options.options)) {
-    if (typeof value === "boolean") {
-      include[key] = value;
-    } else {
-      const next = optionsToInclude(value);
-      include[key] = next[key];
-    }
-  }
-  return { [options.name]: { include: include } } as BaseOptionsReturnType<
-    T,
-    E,
-    U
-  >;
+	if (!options.options) {
+		return { [options.name]: true };
+	}
+	const include: Record<string, unknown> = {};
+	for (const [key, value] of Object.entries(options.options)) {
+		if (typeof value === "boolean") {
+			include[key] = value;
+		} else {
+			const next = optionsToInclude(value);
+			include[key] = next[key];
+		}
+	}
+	return { [options.name]: { include: include } } as BaseOptionsReturnType<
+		T,
+		E,
+		U
+	>;
+};
+
+export type IncludeDataReturnType<
+	T extends BaseOptions,
+	E extends Entity = Entity,
+> = {
+  // [ K in T[ "name" ] ]?: E;
+  [ K in keyof T[ "options" ] ]: IncludeDataReturnType<T[ "options" ][ K ], E>;
 };
